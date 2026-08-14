d3a83b6cb95980045bcd74a55dab0c19133a19c9
devanshborana <devanshborana870@gmail.com>
2026-08-14 22:32:48 +0530

    hi

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -94,9 +94,17 @@ export default function HomePage() {
   const PROGRAM_KEYS = Object.keys(programs)
   const todaysSchedule = weekSchedule[selectedDate.getDay()] || []
 
+  const getGreeting = () => {
+    const hour = new Date().getHours()
+    if (hour < 12) return 'Good Morning'
+    if (hour < 17) return 'Good Afternoon'
+    if (hour < 21) return 'Good Evening'
+    return 'Good Night'
+  }
+
   return (
     <div className="page-content">
-      <h1 className="welcome-heading">Good Morning, {user?.name.split(' ')[0]} 👋</h1>
+      <h1 className="welcome-heading">{getGreeting()}, {user?.name.split(' ')[0]} 👋</h1>
 
       <div className="home-grid">
         {/* LEFT COLUMN */}
